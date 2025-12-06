ae7c2c4f52105784d78039c198dcc8b2fdb5c803
Ronald Cron <ronald.cron@arm.com>
2025-12-06 18:31:42 +0100

    generate_test_driver.py: Define subclass of TestDriverGenerator
    Define the subclass TFPSACryptoTestDriverGenerator
    of TestDriverGenerator and overwrite the
    `get_identifiers_to_prefix` method. The build tree is then
    completely created just by calling `build_tree`.
    
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- tests/scripts/generate_test_driver.py
+++ tests/scripts/generate_test_driver.py
@@ -44,27 +44,50 @@ IDENTIFIER_PREFIXES = {
     "tf_psa_crypto_",
 }
 
-def get_external_identifiers() -> Set[str]:
-    """
-    Get from public and core headers the identifiers that the driver built-in
-    code may reference but does not define.
-    """
-    directories = ("core", "include")
-    files = itertools.chain.from_iterable(Path(directory).rglob("*.h") \
-                                          for directory in directories)
-    identifiers = set()
-    for file in files:
-        identifiers.update(test_driver.run_ctags(file))
+class TFPSACryptoTestDriverGenerator(test_driver.TestDriverGenerator):
+    """ TF-PSA-Crypto test driver generator """
+    def get_identifiers_to_prefix(self, prefixes: Set[str]) -> Set[str]:
+        """
+        Adjust the list of identifiers to prefix in the test driver code
+        """
+        identifiers = super().get_identifiers_to_prefix(prefixes)
 
-    # MBEDTLS_PRIVATE is returned as a prototype by ctags when used in
-    # structure members. Just remove it.
-    identifiers.remove("MBEDTLS_PRIVATE")
+        # Get from public and core headers the identifiers that the driver
+        # built-in code may reference but does not define.
+        directories = ("core", "include")
+        files = itertools.chain.from_iterable(Path(directory).rglob("*.h") \
+                                              for directory in directories)
+        external_identifiers = set()
+        for file in files:
+            external_identifiers.update(self.get_c_identifiers(file))
 
-    # ctags ignores the configuration options that are commented in
-    # crypto_config.h. Ensure we have all of them.
-    identifiers |= set(TFPSACryptoConfig().settings)
+        # MBEDTLS_PRIVATE is returned as a prototype by ctags when used in
+        # structure members. Just remove it.
+        external_identifiers.remove("MBEDTLS_PRIVATE")
 
-    return identifiers
+        # ctags ignores the configuration options that are commented in
+        # crypto_config.h. Ensure we have all of them.
+        external_identifiers |= set(TFPSACryptoConfig().settings)
+
+        identifiers.difference_update(external_identifiers)
+
+        # MBEDTLS_ECP_LIGHT is defined in
+        # tf-psa-crypto/private/crypto_adjust_config_support.h
+        # if MBEDTLS_PK_PARSE_EC_EXTENDED or MBEDTLS_PK_PARSE_EC_COMPRESSED is
+        # enabled. It thus appears in 'external_identifiers' but is a built-in
+        # driver identifier that should be renamed thus force its renaming.
+        identifiers.add("MBEDTLS_ECP_LIGHT")
+
+        # MBEDTLS_PSA_ACCEL_ are not defined in the code base. They are
+        # supposed to be passed as extra configuration options. We want to
+        # prefix them, especially in 'crypto_adjust_config_enable_builtins.h',
+        # thus deduce them from the PSA_WANT_ ones and add them to the list of
+        # identifiers to prefix in the test driver code.
+        for identifier in external_identifiers:
+            if identifier.startswith("PSA_WANT_"):
+                identifiers.add(identifier.replace("PSA_WANT_", "MBEDTLS_PSA_ACCEL_", 1))
+
+        return identifiers
 
 def main():
     """
@@ -76,46 +99,21 @@ def main():
     if not build_tree.looks_like_tf_psa_crypto_root("."):
         raise RuntimeError("This script must be run from TF-PSA-Crypto root.")
 
-    builtin = Path("drivers/builtin")
-    if not builtin.is_dir():
-        raise RuntimeError("Do not find expected drivers/builtin directory.")
-
     dst_dir = Path(args.dst_dir)
     if not dst_dir.is_dir():
-        raise RuntimeError(f"Do not find expected {args.dst_dir} directory.")
+        raise RuntimeError(f"{args.dst_dir} directory not found.")
 
-    generator = test_driver.TestDriverGenerator(builtin, dst_dir, args.driver, \
-                                                EXCLUDE_FILES)
+    generator = TFPSACryptoTestDriverGenerator(Path("drivers/builtin"), dst_dir, \
+                                               args.driver, EXCLUDE_FILES)
+
     if args.list_vars_for_cmake:
         fname = f"{args.driver}-list-vars.cmake" \
                 if args.list_vars_for_cmake == "__AUTO__" else args.list_vars_for_cmake
         generator.write_list_vars_for_cmake(fname)
         return
 
-    #Step 1: Build the test driver tree from `drivers/builtin`
-    generator.build_tree()
-
-    #Step 2: Prefix the identifiers exposed by the built-in driver.
-    identifiers = generator.get_identifiers_with_prefixes(IDENTIFIER_PREFIXES)
-    external_identifiers = get_external_identifiers()
-    identifiers.difference_update(external_identifiers)
-
-    # MBEDTLS_ECP_LIGHT is defined in tf-psa-crypto/private/crypto_adjust_config_support.h
-    # if MBEDTLS_PK_PARSE_EC_EXTENDED or MBEDTLS_PK_PARSE_EC_COMPRESSED is enabled.
-    # It thus appears in 'external_identifiers' but is a built-in driver identifier
-    # that should be renamed thus force its renaming.
-    identifiers.add("MBEDTLS_ECP_LIGHT")
-
-    # MBEDTLS_PSA_ACCEL_ are not defined in the code base. They are supposed to
-    # be passed as extra configuration options. We want to prefix them,
-    # especially in 'crypto_adjust_config_enable_builtins.h', thus deduce them
-    # from the PSA_WANT_ ones and add them to the list of identifiers to
-    # prefix in the test driver code.
-    for identifier in external_identifiers:
-        if identifier.startswith("PSA_WANT_"):
-            identifiers.add(identifier.replace("PSA_WANT_", "MBEDTLS_PSA_ACCEL_", 1))
-
-    generator.prefix_identifiers(identifiers)
+    #Build the test driver tree from `drivers/builtin`
+    generator.build_tree(IDENTIFIER_PREFIXES)
 
 if __name__ == "__main__":
     sys.exit(main())
