6ea6317125f515ac358a74b5df4d57145b859906
Ronald Cron <ronald.cron@arm.com>
2025-12-10 17:16:14 +0100

    test
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- tests/scripts/components-configuration-crypto.sh
+++ tests/scripts/components-configuration-crypto.sh
@@ -10,13 +10,14 @@
 ################################################################
 
 BUILTIN_BUILD_DIR="drivers/builtin/CMakeFiles/builtin.dir/src"
-support_universal_ctags_5_9_0() {
-    version="$(ctags --version 2>/dev/null)" || return 1
-    [[ "$version" == *"Universal Ctags 5.9.0"* ]]
+
+support_ubuntu_version() {
+    version="$(cat /etc/os-release 2>/dev/null)" || return 1
+    [[ "$version" == *$1* ]]
 }
 
-component_test_accel_hash () {
-    msg "test: accelerated hash"
+component_test_accel_hash_ubuntu_16() {
+    msg "test: accelerated hash Ubuntu 16"
 
     # Build
     # -----
@@ -40,6 +41,35 @@ component_test_accel_hash () {
     ctest
 }
 
-support_test_accel_hash () {
-    support_universal_ctags_5_9_0
+support_test_accel_hash_ubuntu_16() {
+    support_ubuntu_version "Ubuntu 16"
+}
+
+component_test_accel_hash_ubuntu_22() {
+    msg "test: accelerated hash Ubuntu 22"
+
+    # Build
+    # -----
+
+    cd $OUT_OF_SOURCE_DIR
+    cmake -DTF_PSA_CRYPTO_TEST_DRIVER=On \
+          -DTF_PSA_CRYPTO_USER_CONFIG_FILE="../tests/configs/user-config-accel-hash.h" ..
+    make
+
+    # Make sure built-in hash objects are empty.
+    not grep mbedtls_md5 ${BUILTIN_BUILD_DIR}/md5.c.o
+    not grep mbedtls_sha1 ${BUILTIN_BUILD_DIR}/sha1.c.o
+    not grep mbedtls_sha256 ${BUILTIN_BUILD_DIR}/sha256.c.o
+    not grep mbedtls_sha512 ${BUILTIN_BUILD_DIR}/sha512.c.o
+    not grep mbedtls_ripemd160 ${BUILTIN_BUILD_DIR}/ripemd160.c.o
+
+    # Run the tests
+    # -------------
+
+    msg "test: accelerated hash"
+    ctest
+}
+
+support_test_accel_hash_ubuntu_22() {
+    support_ubuntu_version "Ubuntu 22"
 }
